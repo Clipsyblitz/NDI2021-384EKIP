39d89686794e3b7ceca6b81c7c4cf60d866d8b0b
malleng <malleng@etu.univ-grenoble-alpes.fr>
2021-12-02 23:19:31 +0100

    changes in peopleSaved.js

--- back/models/peopleSaved.js
+++ back/models/peopleSaved.js
@@ -3,18 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const peopleSavedSchema = new Schema({
-    people : [
-        {
-            name : {
-                type : String,
-                required : true
-            },
-            role : {
-                type : String,
-                required : false
-            }
-        }
-    ]
+    name : {
+        type : String,
+        required : true
+    },
+    role : {
+        type : String,
+        required : false
+    }
 })
 
 module.exports = mongoose.model("PeopleSaved", peopleSavedSchema, "peopleSaved");
