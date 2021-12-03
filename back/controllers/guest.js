856e9b33eb7b7f8650c23cd80974122ac4e3cde9
Fikret Kurklu <fikret.kurklu@etu.univ-grenoble-alpes.fr>
2021-12-03 04:09:32 +0100

    ok

--- back/controllers/guest.js
+++ back/controllers/guest.js
@@ -1,10 +1,13 @@
 const RescueReport = require("../models/rescueReport");
 const SeaTrip = require("../models/seaTrip");
+const Boat = require("../models/boat");
 
 module.exports = {
   getRescueReport: async (req, res, next) => {
     try {
-      const rescueReport = await RescueReport.find();
+      const rescueReport = await RescueReport.find({
+        century: req.params.century,
+      });
 
       return res.status(200).json({
         rescueReport: rescueReport,
@@ -26,6 +29,39 @@ module.exports = {
 
       return res.status(200).json({
         seaTrip: seaTrip,
+      });
+    } catch (error) {
+      console.log(error);
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    }
+  },
+
+  getSearch: async (req, res, next) => {
+    try {
+      const { type, data } = req.params;
+      let result;
+
+      switch (type) {
+        case "boat":
+          console.log(data);
+          const boatResult = await Boat.findOne({ name: data });
+          result = await SeaTrip.find({
+            boat: boatResult._id,
+          });
+          console.log(result);
+          break;
+        case "rescuer":
+          result = await SeaTrip.findOne({ rescuer: data });
+          break;
+        default:
+          console.log("error type");
+      }
+
+      return res.status(200).json({
+        seaTrip: result,
       });
     } catch (error) {
       console.log(error);
