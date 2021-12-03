ab1b197ca17771c5cde787b7ba0c56727e68ae81
Quentin <quentin38590@gmail.com>
2021-12-03 01:37:08 +0100

    Sortie en mer Pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,12 @@
+import SieclePage from "./components/SieclePage";
+
 function App() {
+
+  const date = ["1234-1235","1234-1236","1234-1237"]
+
   return (
-    <div>
-      Hello World !
-    </div>
-  )
+    <SieclePage dates ={date} introduction="Intro ouais"></SieclePage>
+  );
 }
 
 export default App;
